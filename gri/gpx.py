ff82c78b679f955061c064bb50cfcb2d3c23894e
Stephen DuVall <stephen.duvall13@gmail.com>
2023-01-11 22:49:44 -0500

    Created a ton of new functions
    This stuff mostly works

--- gri/gpx.py
+++ gri/gpx.py
@@ -1,5 +1,84 @@
+from math import sin, cos, sqrt, asin, acos, atan2
 import gpxpy
 import gpxpy.gpx
+from geopy.distance import lonlat, distance, Distance
+from geopy.units import rad, degrees
+from geopy import Point
+
+from kml import create_track, create_tracks, Line
+
+
+def n_mile_divisions(segment: gpxpy.gpx.GPXTrackSegment, n_miles=1):
+    total_distance = 0.0
+    current_distance = 0.0
+    n_mile_markers = []
+
+    for i in range(0, len(segment.points) - 1):
+        p1 = (segment.points[i].latitude, segment.points[i].longitude)
+        p2 = (segment.points[i+1].latitude, segment.points[i+1].longitude)
+        pt_distance = distance(p1, p2).miles
+        current_distance += pt_distance
+        total_distance += pt_distance
+
+        if current_distance > n_miles:
+            n_mile_markers.append(segment.points[i+1])
+            current_distance = 0.0
+    return n_mile_markers
+
+
+def calculate_midpoint(point1, point2):
+    """
+    Parameters:
+    point1 (gpxpy.gpx.GPXTrackPoint)
+    point2 (gpxpy.gpx.GPXTrackPoint)
+
+    Returns:
+    midpoint (Point)
+    """
+    del1 = rad(point1.longitude)
+    phi1 = rad(point1.latitude)
+    del2 = rad(point2.longitude)
+    phi2 = rad(point2.latitude)
+    deltadel = del2 - del1
+
+    bx = cos(phi2) * cos(deltadel)
+    by = cos(phi2) * sin(deltadel)
+
+    phi3 = atan2(sin(phi1) + sin(phi2), sqrt((cos(phi1) + bx)**2 + by**2))
+    del3 = del1 + atan2(by, cos(phi1) + bx)
+
+    # return (degrees(del3), degrees(phi3)) # Convert to gpxpy point
+    return Point(degrees(phi3), degrees(del3))
+
+
+def calculate_bearing(point1, point2):
+    """
+    Parameters:
+    point1 (gpxpy.gpx.GPXTrackPoint): Point on original line
+    point2 (Point): Calculated midpoint
+
+    Returns:
+    A bearing from point1 to point2 in degrees
+    """
+    phi1 = point2.latitude
+    del1 = point2.longitude
+    phi2 = point1.latitude
+    del2 = point1.longitude
+    deltadel = del2 - del1
+
+    bearing = atan2(sin(deltadel) * cos(phi2), cos(phi1)*sin(phi2)-sin(phi1)*cos(phi2)*cos(deltadel))
+
+    return degrees(bearing)
+
+
+def calculate_projection(point: gpxpy.gpx.GPXTrackPoint, bearing: float, di: float) -> Point:
+    geopoint = Point(point.latitude, point.longitude)
+    projection = distance(miles=di).destination(point=geopoint, bearing=bearing)
+    return projection
+
+
+def outline_track(track: gpxpy.gpx.GPXTrack):
+    ...
 
 
 def create_gpx(repeaters):
